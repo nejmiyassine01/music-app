9855644886a86f7d125839019d8b7e8c4e830b81
nejmiyassine01 <nejmi.yassine01@gmail.com>
2021-08-05 17:35:32 +0100

    Creating RegisterForm and LoginForm components and validated with vee-validate

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import { createStore } from 'vuex';
+
+export default createStore({
+  state: {
+    authModalShow: false,
+  },
+  mutations: {
+    toggleAuthModal: (state) => {
+      state.authModalShow = !state.authModalShow;
+    },
+  },
+  // getters: { authModalShow: (state) => state.authModalShow },
+});
