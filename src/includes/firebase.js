3961168ef2fe00ab17d0073bd21c0179d104f03d
nejmiyassine01 <nejmi.yassine01@gmail.com>
2021-08-07 19:44:39 +0100

    Finish the Uploading Files Music

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBQbfb4VZIyLT5cO8fQRst2qXPu3836MYo',
@@ -15,7 +16,11 @@ firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const db = firebase.firestore();
+const storage = firebase.storage();
 
 const usersCollection = db.collection('users');
+const songsCollection = db.collection('songs');
 
-export { auth, db, usersCollection };
+export {
+  auth, db, storage, songsCollection, usersCollection,
+};
