b2d24ff2706fee7624b129fd3e5cc79edab9e3f3
nejmiyassine01 <nejmi.yassine01@gmail.com>
2021-08-09 18:47:42 +0100

    Adding Play/Stop Functionalities with transition between switching pages

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
 import About from '@/views/About.vue';
 import Manage from '@/views/Manage.vue';
-import Song from '@/components/Song.vue';
+import Song from '@/views/Song.vue';
 import store from '@/store';
 
 const routes = [
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,17 @@
 import { createStore } from 'vuex';
 import { auth, usersCollection } from '@/includes/firebase';
+import { Howl } from 'howler';
+import helpers from '@/includes/helpers';
 
 export default createStore({
   state: {
     authModalShow: false,
     userLoggedIn: false,
+    currentSong: {},
+    sound: {},
+    seek: '00:00',
+    duration: '00:00',
+    playerProgress: '0%',
   },
   mutations: {
     toggleAuthModal: (state) => {
@@ -13,8 +20,28 @@ export default createStore({
     toggleAuth: (state) => {
       state.userLoggedIn = !state.userLoggedIn;
     },
+    newSong: (state, payload) => {
+      state.currentSong = payload;
+      state.sound = new Howl({
+        src: [payload.url],
+        html5: true,
+      });
+    },
+    updatePosition(state) {
+      state.seek = helpers.formatTime(state.sound.seek());
+      state.duration = helpers.formatTime(state.sound.duration());
+      state.playerProgress = `${(state.sound.seek() / state.sound.duration())
+        * 100}%`;
+    },
   },
-  // getters: { authModalShow: (state) => state.authModalShow },
+  getters: {
+    playing: (state) => {
+      if (state.sound.playing) {
+        return state.sound.playing();
+      }
+      return false;
+    },
+  },
   actions: {
     async register({ commit }, payload) {
       const userCred = await auth.createUserWithEmailAndPassword(
@@ -55,6 +82,57 @@ export default createStore({
       if (payload.route.meta.requiresAuth) {
         payload.router.push({ name: 'home' });
       }
+    },
+    async newSong({ commit, state, dispatch }, payload) {
+      if (state.sound instanceof Howl) {
+        state.sound.unload();
+      }
+
+      commit('newSong', payload);
+
+      state.sound.play();
+
+      state.sound.on('play', () => {
+        requestAnimationFrame(() => {
+          dispatch('progress');
+        });
+      });
+    },
+    async toggleAudio({ state }) {
+      if (!state.sound.playing) {
+        return;
+      }
+
+      if (state.sound.playing()) {
+        state.sound.pause();
+      } else {
+        state.sound.play();
+      }
+    },
+    progress({ commit, state, dispatch }) {
+      commit('updatePosition');
+
+      if (state.sound.playing()) {
+        requestAnimationFrame(() => {
+          dispatch('progress');
+        });
+      }
+    },
+    updateSeek({ state, dispatch }, payload) {
+      if (!state.sound.playing) {
+        return;
+      }
+
+      const { x, width } = payload.currentTarget.getBoundingClientRect();
+      const clickX = payload.clientX - x;
+      const percentage = clickX / width;
+      const seconds = state.sound.duration() * percentage;
+
+      state.sound.seek(seconds);
+
+      state.sound.once('seek', () => {
+        dispatch('progress');
+      });
     },
   },
 });
