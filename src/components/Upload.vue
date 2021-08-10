ff4e33a6d9b5f0b11b42f40136dce9daeb1250dc
nejmiyassine01 <nejmi.yassine01@gmail.com>
2021-08-10 17:36:26 +0100

    Adding The pwa to the app

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -63,6 +63,19 @@ export default {
           return;
         }
 
+        if (!navigator.online) {
+          this.upload.push({
+            task: {},
+            current_progress: 100,
+            name: file.name,
+            variant: 'bg-red-400',
+            icon: 'fas fa-time',
+            text_class: 'text-red-400',
+          });
+
+          return;
+        }
+
         const storageRef = storage.ref(); // music-app-4444c.appspot.com
         const songsRef = storageRef.child(`songs/${file.name}`); // music-app-4444c.appspot.com/songs/example.mp3
         const task = songsRef.put(file);
