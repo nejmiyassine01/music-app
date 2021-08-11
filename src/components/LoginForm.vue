d1c2fe9a93f594e1898425b851694459d5e34bd9
nejmiyassine01 <nejmi.yassine01@gmail.com>
2021-08-11 12:46:59 +0100

    Remove log from login components

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -59,7 +59,6 @@ export default {
   },
   methods: {
     async login(values) {
-      console.log(values);
       this.login_in_submission = true;
       this.login_show_alert = true;
       this.login_alert_variant = 'bg-blue-500';
